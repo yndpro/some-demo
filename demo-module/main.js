f6eef7b5101b8ae1437f9383929384454860afb2
Adrian <adrianyang.hk@gamil.com>
2018-09-03 00:38:36 +0800

    a

--- demo-module/main.js
+++ demo-module/main.js
@@ -19,10 +19,6 @@ console.log(test)
 
 
 
-// CommonJS 扩展了JavaScript声明模块的API.
-// 浏览器不兼容CommonJS的根本原因，在于缺少四个Node.js环境的变量 (module  exports  require  global)
-// 同步载入模块
 
-// CommonJS创建了两份模块的实例 一个在导出的时候，一个在引入的时候
 
 
