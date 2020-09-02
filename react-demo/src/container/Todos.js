3fb95d1702037b23640ff326af50b1637d4f24ac
Adrian <yanglinde@4399inc.com>
2020-09-02 19:36:47 +0800

    s

--- react-demo/src/container/Todos.js
+++ react-demo/src/container/Todos.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import Todos from "../components/Todos";
 
+const mapStateToProps = state => ({
+    todos : state.todos
+})
 
+const mapDispatchToProps = dispatch => ({
+    setFilter : filter => dispatch(filter)
+})
+
+const Todos = connect(
+    mapStateToProps,
+)(Todos)
 
 export default Todos;
